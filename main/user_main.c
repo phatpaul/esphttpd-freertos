f91dce5aabc08d7e68ec31a4ebfadb7d39a775d7
Paul Abbott <labview1@lumitec>
2020-01-07 14:33:23 -0500

    Assertion failure when switching to station only mode #19 Also use NVS to store WiFi settings (optional).  Improved WiFi status indicator LED function.

--- main/user_main.c
+++ main/user_main.c
@@ -44,12 +44,18 @@ the server, including WiFi connection management capabilities, some IO etc.
 #include "freertos/event_groups.h"
 #include "esp_log.h"
 
+#define CONFIG_STORE_WIFI_TO_NVS
+#ifdef CONFIG_STORE_WIFI_TO_NVS
 #include "nvs_flash.h"
+#define NVS_NAMESPACE "nvs"
+nvs_handle my_nvs_handle;
+#define NET_CONF_KEY "netconf"
+#endif
+
 #include "esp_err.h"
 #include "esp_event_loop.h"
 #include "esp_event.h"
 #include "tcpip_adapter.h"
-
 
 char my_hostname[16] = "esphttpd";
 
@@ -57,10 +63,11 @@ char my_hostname[16] = "esphttpd";
    'make menuconfig'.
 
    If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+   the config you want - ie #define DEFAULT_WIFI_STA_SSID "mywifissid"
 */
-#define EXAMPLE_WIFI_SSID      CONFIG_EXAMPLE_WIFI_SSID
-#define EXAMPLE_WIFI_PASS      CONFIG_EXAMPLE_WIFI_PASSWORD
+#define DEFAULT_WIFI_MODE WIFI_MODE_AP
+#define DEFAULT_WIFI_STA_SSID CONFIG_EXAMPLE_WIFI_SSID
+#define DEFAULT_WIFI_STA_PASS CONFIG_EXAMPLE_WIFI_PASSWORD
 #endif
 
 #define TAG "user_main"
@@ -203,6 +210,46 @@ HttpdBuiltInUrl builtInUrls[] = {
 
 #ifdef ESP32
 
+static void update_status_ind_wifi()
+{
+	esp_err_t result;
+	wifi_mode_t api_wifi_mode;
+
+	result = esp_wifi_get_mode(&api_wifi_mode);
+	if (result != ESP_OK)
+	{
+		ESP_LOGE(TAG, "[%s] Error fetching WiFi mode.", __FUNCTION__);
+		return;
+	}
+
+	if (api_wifi_mode == WIFI_MODE_NULL)
+	{
+		set_status_ind_wifi(WIFI_STATE_OFF);
+		return;
+	}
+	if (api_wifi_mode == WIFI_MODE_STA || api_wifi_mode == WIFI_MODE_APSTA)
+	{
+		wifi_ap_record_t ap_info;
+		if (esp_wifi_sta_get_ap_info(&ap_info) == ESP_OK)
+		{
+			set_status_ind_wifi(WIFI_STATE_CONN);
+			return;
+		}
+	}
+	if (api_wifi_mode == WIFI_MODE_AP || api_wifi_mode == WIFI_MODE_APSTA)
+	{
+		wifi_sta_list_t sta_list;
+		if (esp_wifi_ap_get_sta_list(&sta_list) == ESP_OK)
+		{
+			if (sta_list.num > 0) // at least one client connected?
+			{
+				set_status_ind_wifi(WIFI_STATE_CONN);
+				return;
+			}
+		}
+	}
+	set_status_ind_wifi(WIFI_STATE_IDLE);
+}
 
 static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 {
@@ -230,7 +277,6 @@ static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 		}
 		printf("~~~~~~~~~~~~~" "\n");
 	}
-	set_status_ind_wifi(WIFI_STATE_CONN);
 	break;
 	case SYSTEM_EVENT_STA_CONNECTED:
 		break;
@@ -265,7 +311,7 @@ static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 				printf("GW:" IPSTR "\n", IP2STR(&ap_ip_info.gw));
 			}
 			printf("~~~~~~~~~~~~" "\n");
-			set_status_ind_wifi(WIFI_STATE_AP);
+			set_status_ind_wifi(WIFI_STATE_IDLE);
 		}
 		break;
 		case SYSTEM_EVENT_AP_STACONNECTED:
@@ -273,16 +319,12 @@ static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 					MAC2STR(event->event_info.sta_connected.mac),
 					event->event_info.sta_connected.aid);
 
-			set_status_ind_wifi(WIFI_STATE_CONN);
 			break;
 		case SYSTEM_EVENT_AP_STADISCONNECTED:
 			ESP_LOGI(TAG, "station:" MACSTR"leave,AID=%d",
 					MAC2STR(event->event_info.sta_disconnected.mac),
 					event->event_info.sta_disconnected.aid);
 
-			wifi_sta_list_t sta_list;
-			ESP_ERROR_CHECK( esp_wifi_ap_get_sta_list(&sta_list));
-			if (sta_list.num == 0) set_status_ind_wifi(WIFI_STATE_AP); // no clients left, change blink indication to AP
 			break;
 		case SYSTEM_EVENT_SCAN_DONE:
 
@@ -293,62 +335,100 @@ static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 
 	/* Forward event to to the WiFi CGI module */
 	cgiWifiEventCb(event);
+	update_status_ind_wifi();
 
 	return ESP_OK;
 }
 
-
-//Simple task to connect to an access point
-void init_wifi(bool modeAP) {
-	esp_err_t result;
-
-	result = nvs_flash_init();
-	if(   result == ESP_ERR_NVS_NO_FREE_PAGES
-	   || result == ESP_ERR_NVS_NEW_VERSION_FOUND)
+void init_wifi(bool factory_defaults)
 	{
-		ESP_LOGI(TAG, "Erasing NVS");
-		nvs_flash_erase();
-		result = nvs_flash_init();
-	}
-	ESP_ERROR_CHECK(result);
-
-	// Initialise wifi configuration CGI
-	result = initCgiWifi();
-	ESP_ERROR_CHECK(result);
-
-	ESP_ERROR_CHECK( esp_event_loop_init(app_event_handler, NULL) );
-
+	wifi_mode_t old_mode;
 	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-	//Go to station mode
-	esp_wifi_disconnect();
+	// Try to get WiFi configuration from NVS?
+#if defined(CONFIG_STORE_WIFI_TO_NVS)
+	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_FLASH)); // WIFI_STORAGE_FLASH or WIFI_STORAGE_RAM
+#else														   // don't save WiFi config to NVS
+	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+#endif
+	ESP_ERROR_CHECK(esp_wifi_get_mode(&old_mode));
 
-	if(modeAP) {
-		ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_AP) );
-
-		wifi_config_t ap_config;
-		strcpy((char*)(&ap_config.ap.ssid), "ESP");
-		ap_config.ap.ssid_len = 3;
-		ap_config.ap.channel = 1;
-		ap_config.ap.authmode = WIFI_AUTH_OPEN;
-		ap_config.ap.ssid_hidden = 0;
-		ap_config.ap.max_connection = 3;
-		ap_config.ap.beacon_interval = 100;
-
-		esp_wifi_set_config(WIFI_IF_AP, &ap_config);
+	if (factory_defaults)
+	{
+		old_mode = DEFAULT_WIFI_MODE;
 	}
-	else {
-		esp_wifi_set_mode(WIFI_MODE_STA);
 
-		//Connect to the defined access point.
-		wifi_config_t config;
-		memset(&config, 0, sizeof(config));
-		sprintf((char*)config.sta.ssid, EXAMPLE_WIFI_SSID);
-		sprintf((char*)config.sta.password, EXAMPLE_WIFI_PASS);
-		esp_wifi_set_config(WIFI_IF_STA, &config);
-		esp_wifi_connect();
+	if (old_mode == WIFI_MODE_APSTA || old_mode == WIFI_MODE_STA)
+	{
+		//// STA settings
+		wifi_config_t factory_sta_config = {
+			.sta = {
+				.ssid = DEFAULT_WIFI_STA_SSID,
+				.password = DEFAULT_WIFI_STA_PASS,
+			}};
+		wifi_config_t sta_stored_config;
+		//esp_wifi_set_mode(WIFI_MODE_APSTA); // must enable modes before trying esp_wifi_get_config()
+		ESP_ERROR_CHECK(esp_wifi_get_config(ESP_IF_WIFI_STA, &sta_stored_config));
+
+		if (factory_defaults && strlen((char *)factory_sta_config.sta.ssid) != 0)
+		{
+			// load factory default STA config
+			ESP_LOGI(TAG, "Using factory-default WiFi STA configuration, ssid: %s", factory_sta_config.sta.ssid);
+			ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &factory_sta_config));
+		}
+		else if (strlen((char *)sta_stored_config.sta.ssid) != 0)
+		{
+			ESP_LOGI(TAG, "Using WiFi STA configuration from NVS, ssid: %s", sta_stored_config.sta.ssid);
+			ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_stored_config));
+		}
+		else
+		{
+			ESP_LOGW(TAG, "No WiFi STA configuration available");
+			if (old_mode == WIFI_MODE_APSTA)
+				esp_wifi_set_mode(WIFI_MODE_AP); // remove STA mode
+			if (old_mode == WIFI_MODE_STA)
+				esp_wifi_set_mode(WIFI_MODE_NULL); // remove STA mode
+		}
+	}
+
+	if (old_mode == WIFI_MODE_APSTA || old_mode == WIFI_MODE_AP)
+	{
+		//// AP settings
+		wifi_config_t factory_ap_config;
+		{
+			strncpy((char *)(&factory_ap_config.ap.ssid), "ESP", (sizeof((wifi_ap_config_t *)0)->ssid));
+			factory_ap_config.ap.ssid_len = 0; // 0: use null termination to determine size
+			factory_ap_config.ap.channel = 6;
+			factory_ap_config.ap.authmode = WIFI_AUTH_OPEN; //WIFI_AUTH_WPA_WPA2_PSK; //WIFI_AUTH_OPEN;
+			//strncpy((char *)(&factory_ap_config.ap.password), DEFAULT_WIFI_AP_PASS, (sizeof((wifi_ap_config_t *)0)->password));
+			factory_ap_config.ap.ssid_hidden = 0;
+			factory_ap_config.ap.max_connection = 4;
+			factory_ap_config.ap.beacon_interval = 100;
+		}
+		wifi_config_t ap_stored_config;
+		ESP_ERROR_CHECK(esp_wifi_get_config(ESP_IF_WIFI_AP, &ap_stored_config));
+
+		if (factory_defaults && strlen((char *)factory_ap_config.ap.ssid) != 0)
+		{
+
+			// load factory default STA config
+			ESP_LOGI(TAG, "Using factory-default WiFi AP configuration, ssid: %s", factory_ap_config.ap.ssid);
+			ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &factory_ap_config));
+		}
+		else if (strlen((char *)ap_stored_config.ap.ssid) != 0)
+		{
+			ESP_LOGI(TAG, "Using WiFi AP configuration from NVS, ssid: %s", ap_stored_config.ap.ssid);
+			ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_stored_config));
+	}
+	else
+	{
+			ESP_LOGW(TAG, "No WiFi AP configuration available");
+			if (old_mode == WIFI_MODE_APSTA)
+				esp_wifi_set_mode(WIFI_MODE_STA); // remove AP mode
+			if (old_mode == WIFI_MODE_AP)
+				esp_wifi_set_mode(WIFI_MODE_NULL); // remove AP mode
+	}
 	}
 
 	ESP_ERROR_CHECK( esp_wifi_start() );
@@ -369,6 +449,43 @@ void user_init(void) {
 	ioInit();
 // FIXME: Re-enable this when capdns is fixed for esp32
 //	captdnsInit();
+	esp_err_t err;
+	// Init NVS
+	err = nvs_flash_init();
+	if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+	{
+		// NVS partition was truncated and needs to be erased
+		// Retry nvs_flash_init
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		err = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(err);
+
+	uint32_t net_configured = 0;	 // value will default to 0, if not set yet in NVS
+	ESP_LOGI(TAG, "Opening NVS handle ");
+	err = nvs_open(NVS_NAMESPACE, NVS_READWRITE, &my_nvs_handle);
+	if (err != ESP_OK)
+	{
+		ESP_LOGE(TAG, "Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+	}
+	else
+	{
+
+		// Read NVS
+		ESP_LOGI(TAG, "Reading network initialization from NVS");
+		err = nvs_get_u32(my_nvs_handle, NET_CONF_KEY, &net_configured);
+		switch (err)
+		{
+		case ESP_OK:
+			ESP_LOGI(TAG, "nvs init = %d", net_configured);
+			break;
+		case ESP_ERR_NVS_NOT_FOUND:
+			ESP_LOGI(TAG, "nvs init not found, initializing now.");
+			break;
+		default:
+			ESP_LOGE(TAG, "Error (%s) reading!\n", esp_err_to_name(err));
+		}
+	}
 
 #ifdef CONFIG_ESPHTTPD_USE_ESPFS
 	espFsInit((void*)(image_espfs_start));
@@ -383,9 +500,26 @@ void user_init(void) {
 	                  HTTPD_FLAG_NONE);
 	httpdFreertosStart(&httpdFreertosInstance);
 
-	init_wifi(true); // Supply false for STA mode
 
 	xTaskCreate(websocketBcast, "wsbcast", 3000, NULL, 3, NULL);
+	
+	ESP_ERROR_CHECK(initCgiWifi()); // Initialise wifi configuration CGI
+
+	ESP_ERROR_CHECK(esp_event_loop_init(app_event_handler, NULL));
+
+	init_wifi(!net_configured); // Start Wifi, restore factory wifi settings if not initialized
+
+	if (!net_configured)
+	{ // If wasn't initialized, now we are initialized.  Write it to NVS.
+		net_configured = 1;
+		ESP_LOGI(TAG, "Writing init to NVS");
+		ESP_ERROR_CHECK(nvs_set_u32(my_nvs_handle, NET_CONF_KEY, net_configured));
+		// After setting any values, nvs_commit() must be called to ensure changes are written to flash storage.
+		ESP_LOGI(TAG, "Committing updates in NVS");
+		ESP_ERROR_CHECK(nvs_commit(my_nvs_handle));
+		// Close NVS
+		//nvs_close(my_nvs_handle); - don't close if handle is shared by cgi_NVS
+	}
 
 	printf("\nReady\n");
 }
