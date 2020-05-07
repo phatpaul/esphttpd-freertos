7e5b19a637e2960995537f9a23487c17322be971
Paul Abbott <pabbott@lumiteclighting.com>
2020-05-06 21:23:59 -0400

    improved WiFi STA connection robustness.  To be used with libesphttpd/pull/85 Also formatted with VS-Code.

--- main/user_main.c
+++ main/user_main.c
@@ -77,7 +77,7 @@ char my_hostname[16] = "esphttpd";
 
 #define TAG "user_main"
 
-#define LISTEN_PORT     80u
+#define LISTEN_PORT 80u
 #define MAX_CONNECTIONS 32u
 
 static char connectionMemory[sizeof(RtosConnType) * MAX_CONNECTIONS];
@@ -86,78 +86,84 @@ static HttpdFreertosInstance httpdFreertosInstance;
 //Function that tells the authentication system what users/passwords live on the system.
 //This is disabled in the default build; if you want to try it, enable the authBasic line in
 //the builtInUrls below.
-int myPassFn(HttpdConnData *connData, int no, char *user, int userLen, char *pass, int passLen) {
-	if (no==0) {
+int myPassFn(HttpdConnData *connData, int no, char *user, int userLen, char *pass, int passLen)
+{
+	if (no == 0)
+	{
 		strcpy(user, "admin");
 		strcpy(pass, "s3cr3t");
 		return 1;
-//Add more users this way. Check against incrementing no for each user added.
-//	} else if (no==1) {
-//		strcpy(user, "user1");
-//		strcpy(pass, "something");
-//		return 1;
+		//Add more users this way. Check against incrementing no for each user added.
+		//	} else if (no==1) {
+		//		strcpy(user, "user1");
+		//		strcpy(pass, "something");
+		//		return 1;
 	}
 	return 0;
 }
 
-
 //Broadcast the uptime in seconds every second over connected websockets
-static void websocketBcast(void *arg) {
-	static int ctr=0;
+static void websocketBcast(void *arg)
+{
+	static int ctr = 0;
 	char buff[128];
-	while(1) {
+	while (1)
+	{
 		ctr++;
-		sprintf(buff, "Up for %d minutes %d seconds!\n", ctr/60, ctr%60);
+		sprintf(buff, "Up for %d minutes %d seconds!\n", ctr / 60, ctr % 60);
 		cgiWebsockBroadcast(&httpdFreertosInstance.httpdInstance,
-		                    "/websocket/ws.cgi", buff, strlen(buff),
-		                    WEBSOCK_FLAG_NONE);
+							"/websocket/ws.cgi", buff, strlen(buff),
+							WEBSOCK_FLAG_NONE);
 
-		vTaskDelay(1000/portTICK_RATE_MS);
+		vTaskDelay(1000 / portTICK_RATE_MS);
 	}
 }
 
 //On reception of a message, send "You sent: " plus whatever the other side sent
-static void myWebsocketRecv(Websock *ws, char *data, int len, int flags) {
+static void myWebsocketRecv(Websock *ws, char *data, int len, int flags)
+{
 	int i;
 	char buff[128];
 	sprintf(buff, "You sent: ");
-	for (i=0; i<len; i++) buff[i+10]=data[i];
-	buff[i+10]=0;
+	for (i = 0; i < len; i++)
+		buff[i + 10] = data[i];
+	buff[i + 10] = 0;
 	cgiWebsocketSend(&httpdFreertosInstance.httpdInstance,
-	                 ws, buff, strlen(buff), WEBSOCK_FLAG_NONE);
+					 ws, buff, strlen(buff), WEBSOCK_FLAG_NONE);
 }
 
 //Websocket connected. Install reception handler and send welcome message.
-static void myWebsocketConnect(Websock *ws) {
-	ws->recvCb=myWebsocketRecv;
+static void myWebsocketConnect(Websock *ws)
+{
+	ws->recvCb = myWebsocketRecv;
 	cgiWebsocketSend(&httpdFreertosInstance.httpdInstance,
-	                 ws, "Hi, Websocket!", 14, WEBSOCK_FLAG_NONE);
+					 ws, "Hi, Websocket!", 14, WEBSOCK_FLAG_NONE);
 }
 
 //On reception of a message, echo it back verbatim
-void myEchoWebsocketRecv(Websock *ws, char *data, int len, int flags) {
+void myEchoWebsocketRecv(Websock *ws, char *data, int len, int flags)
+{
 	printf("EchoWs: echo, len=%d\n", len);
 	cgiWebsocketSend(&httpdFreertosInstance.httpdInstance,
-	                 ws, data, len, flags);
+					 ws, data, len, flags);
 }
 
 //Echo websocket connected. Install reception handler.
-void myEchoWebsocketConnect(Websock *ws) {
+void myEchoWebsocketConnect(Websock *ws)
+{
 	printf("EchoWs: connect\n");
-	ws->recvCb=myEchoWebsocketRecv;
+	ws->recvCb = myEchoWebsocketRecv;
 }
 
 #define OTA_FLASH_SIZE_K 1024
 #define OTA_TAGNAME "generic"
 
-CgiUploadFlashDef uploadParams={
-	.type=CGIFLASH_TYPE_FW,
-	.fw1Pos=0x1000,
-	.fw2Pos=((OTA_FLASH_SIZE_K*1024)/2)+0x1000,
-	.fwSize=((OTA_FLASH_SIZE_K*1024)/2)-0x1000,
-	.tagName=OTA_TAGNAME
-};
-
+CgiUploadFlashDef uploadParams = {
+	.type = CGIFLASH_TYPE_FW,
+	.fw1Pos = 0x1000,
+	.fw2Pos = ((OTA_FLASH_SIZE_K * 1024) / 2) + 0x1000,
+	.fwSize = ((OTA_FLASH_SIZE_K * 1024) / 2) - 0x1000,
+	.tagName = OTA_TAGNAME};
 
 /*
 This is the main url->function dispatching data struct.
@@ -209,9 +215,7 @@ HttpdBuiltInUrl builtInUrls[] = {
 
 	ROUTE_FILESYSTEM(),
 
-	ROUTE_END()
-};
-
+	ROUTE_END()};
 
 #ifdef ESP32
 
@@ -258,7 +262,8 @@ static void update_status_ind_wifi()
 
 static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 {
-	switch(event->event_id) {
+	switch (event->event_id)
+	{
 	case SYSTEM_EVENT_ETH_START:
 		tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_ETH, my_hostname);
 		break;
@@ -270,27 +275,33 @@ static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 	{
 		tcpip_adapter_ip_info_t sta_ip_info;
 		wifi_config_t sta_conf;
-		printf("~~~~~STA~~~~~" "\n");
-		if (esp_wifi_get_config(TCPIP_ADAPTER_IF_STA, &sta_conf) == ESP_OK) {
-			printf("ssid: %s" "\n", sta_conf.sta.ssid);
+		printf("~~~~~STA~~~~~"
+			   "\n");
+		if (esp_wifi_get_config(TCPIP_ADAPTER_IF_STA, &sta_conf) == ESP_OK)
+		{
+			printf("ssid: %s"
+				   "\n",
+				   sta_conf.sta.ssid);
 		}
 
-		if (tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &sta_ip_info) == ESP_OK) {
+		if (tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &sta_ip_info) == ESP_OK)
+		{
 			printf("IP:" IPSTR "\n", IP2STR(&sta_ip_info.ip));
 			printf("MASK:" IPSTR "\n", IP2STR(&sta_ip_info.netmask));
 			printf("GW:" IPSTR "\n", IP2STR(&sta_ip_info.gw));
 		}
-		printf("~~~~~~~~~~~~~" "\n");
+		printf("~~~~~~~~~~~~~"
+			   "\n");
 	}
 	break;
 	case SYSTEM_EVENT_STA_CONNECTED:
 		break;
 	case SYSTEM_EVENT_STA_DISCONNECTED:
-		/* This is a workaround as ESP32 WiFi libs don't currently
-           auto-reassociate. */
-		/* Skip reconnect if disconnect was deliberate or authentication      *\
-        \* failed.                                                            */
-		switch(event->event_info.disconnected.reason){
+		/* This is a workaround as ESP32 WiFi libs don't currently auto-reassociate. */
+		/* Skip reconnect if disconnect was deliberate or authentication failed.     */
+		/* this is now handled by cgiwifi watchdog
+		switch (event->event_info.disconnected.reason)
+		{
 		case WIFI_REASON_ASSOC_LEAVE:
 		case WIFI_REASON_AUTH_FAIL:
 			break;
@@ -298,44 +309,53 @@ static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 			esp_wifi_connect();
 			break;
 		}
+		*/
 		break;
-		case SYSTEM_EVENT_AP_START:
+	case SYSTEM_EVENT_AP_START:
+	{
+		tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_AP, my_hostname);
+		tcpip_adapter_ip_info_t ap_ip_info;
+		wifi_config_t ap_conf;
+		printf("~~~~~AP~~~~~"
+			   "\n");
+		if (esp_wifi_get_config(TCPIP_ADAPTER_IF_AP, &ap_conf) == ESP_OK)
 		{
-			tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_AP, my_hostname);
-			tcpip_adapter_ip_info_t ap_ip_info;
-			wifi_config_t ap_conf;
-			printf("~~~~~AP~~~~~" "\n");
-			if (esp_wifi_get_config(TCPIP_ADAPTER_IF_AP, &ap_conf) == ESP_OK) {
-				printf("ssid: %s" "\n", ap_conf.ap.ssid);
-				if (ap_conf.ap.authmode != WIFI_AUTH_OPEN) printf("pass: %s" "\n", ap_conf.ap.password);
-			}
-
-			if (tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_AP, &ap_ip_info) == ESP_OK) {
-				printf("IP:" IPSTR "\n", IP2STR(&ap_ip_info.ip));
-				printf("MASK:" IPSTR "\n", IP2STR(&ap_ip_info.netmask));
-				printf("GW:" IPSTR "\n", IP2STR(&ap_ip_info.gw));
-			}
-			printf("~~~~~~~~~~~~" "\n");
-			set_status_ind_wifi(WIFI_STATE_IDLE);
+			printf("ssid: %s"
+				   "\n",
+				   ap_conf.ap.ssid);
+			if (ap_conf.ap.authmode != WIFI_AUTH_OPEN)
+				printf("pass: %s"
+					   "\n",
+					   ap_conf.ap.password);
 		}
+
+		if (tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_AP, &ap_ip_info) == ESP_OK)
+		{
+			printf("IP:" IPSTR "\n", IP2STR(&ap_ip_info.ip));
+			printf("MASK:" IPSTR "\n", IP2STR(&ap_ip_info.netmask));
+			printf("GW:" IPSTR "\n", IP2STR(&ap_ip_info.gw));
+		}
+		printf("~~~~~~~~~~~~"
+			   "\n");
+	}
+	break;
+	case SYSTEM_EVENT_AP_STACONNECTED:
+		ESP_LOGI(TAG, "station:" MACSTR " join,AID=%d",
+				 MAC2STR(event->event_info.sta_connected.mac),
+				 event->event_info.sta_connected.aid);
+
 		break;
-		case SYSTEM_EVENT_AP_STACONNECTED:
-			ESP_LOGI(TAG, "station:" MACSTR" join,AID=%d",
-					MAC2STR(event->event_info.sta_connected.mac),
-					event->event_info.sta_connected.aid);
+	case SYSTEM_EVENT_AP_STADISCONNECTED:
+		ESP_LOGI(TAG, "station:" MACSTR "leave,AID=%d",
+				 MAC2STR(event->event_info.sta_disconnected.mac),
+				 event->event_info.sta_disconnected.aid);
 
-			break;
-		case SYSTEM_EVENT_AP_STADISCONNECTED:
-			ESP_LOGI(TAG, "station:" MACSTR"leave,AID=%d",
-					MAC2STR(event->event_info.sta_disconnected.mac),
-					event->event_info.sta_disconnected.aid);
+		break;
+	case SYSTEM_EVENT_SCAN_DONE:
 
-			break;
-		case SYSTEM_EVENT_SCAN_DONE:
-
-			break;
-		default:
-			break;
+		break;
+	default:
+		break;
 	}
 #ifdef ETHERNET_ENABLE
 	ethernet_handle_system_event(ctx, event);
@@ -349,35 +369,38 @@ static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 }
 
 void init_wifi(bool factory_defaults)
-	{
+{
 	wifi_mode_t old_mode;
 	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
 	// Try to get WiFi configuration from NVS?
 #if defined(CONFIG_STORE_WIFI_TO_NVS)
 	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_FLASH)); // WIFI_STORAGE_FLASH or WIFI_STORAGE_RAM
 #else														   // don't save WiFi config to NVS
-	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
 #endif
 	ESP_ERROR_CHECK(esp_wifi_get_mode(&old_mode));
 
 	if (factory_defaults)
 	{
 		old_mode = DEFAULT_WIFI_MODE;
+		esp_wifi_set_mode(old_mode);
 	}
 
 	if (old_mode == WIFI_MODE_APSTA || old_mode == WIFI_MODE_STA)
 	{
+		ESP_ERROR_CHECK(esp_wifi_set_bandwidth(WIFI_IF_STA, WIFI_BW_HT20)); // set 20MHz bandwidth, for better range
 		//// STA settings
 		wifi_config_t factory_sta_config = {
 			.sta = {
 				.ssid = DEFAULT_WIFI_STA_SSID,
 				.password = DEFAULT_WIFI_STA_PASS,
-			}};
+				.sort_method = WIFI_CONNECT_AP_BY_SIGNAL}};
 		wifi_config_t sta_stored_config;
-		//esp_wifi_set_mode(WIFI_MODE_APSTA); // must enable modes before trying esp_wifi_get_config()
+
 		ESP_ERROR_CHECK(esp_wifi_get_config(ESP_IF_WIFI_STA, &sta_stored_config));
+		sta_stored_config.sta.sort_method = WIFI_CONNECT_AP_BY_SIGNAL;
 
 		if (factory_defaults && strlen((char *)factory_sta_config.sta.ssid) != 0)
 		{
@@ -402,6 +425,7 @@ void init_wifi(bool factory_defaults)
 
 	if (old_mode == WIFI_MODE_APSTA || old_mode == WIFI_MODE_AP)
 	{
+		ESP_ERROR_CHECK(esp_wifi_set_bandwidth(WIFI_IF_AP, WIFI_BW_HT20)); // set 20MHz bandwidth, for better range
 		//// AP settings
 		wifi_config_t factory_ap_config;
 		{
@@ -413,41 +437,42 @@ void init_wifi(bool factory_defaults)
 			factory_ap_config.ap.ssid_hidden = 0;
 			factory_ap_config.ap.max_connection = 4;
 			factory_ap_config.ap.beacon_interval = 100;
-	}
+		}
 		wifi_config_t ap_stored_config;
 		ESP_ERROR_CHECK(esp_wifi_get_config(ESP_IF_WIFI_AP, &ap_stored_config));
 
 		if (factory_defaults && strlen((char *)factory_ap_config.ap.ssid) != 0)
 		{
-
 			// load factory default STA config
 			ESP_LOGI(TAG, "Using factory-default WiFi AP configuration, ssid: %s", factory_ap_config.ap.ssid);
 			ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &factory_ap_config));
-	}
+		}
 		else if (strlen((char *)ap_stored_config.ap.ssid) != 0)
 		{
 			ESP_LOGI(TAG, "Using WiFi AP configuration from NVS, ssid: %s", ap_stored_config.ap.ssid);
 			ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_stored_config));
-	}
-	else
-	{
+		}
+		else
+		{
 			ESP_LOGW(TAG, "No WiFi AP configuration available");
 			if (old_mode == WIFI_MODE_APSTA)
 				esp_wifi_set_mode(WIFI_MODE_STA); // remove AP mode
 			if (old_mode == WIFI_MODE_AP)
 				esp_wifi_set_mode(WIFI_MODE_NULL); // remove AP mode
-	}
+		}
 	}
 
-	ESP_ERROR_CHECK( esp_wifi_start() );
+	ESP_ERROR_CHECK(esp_wifi_start());
 }
 #endif
 
 //Main routine. Initialize stdout, the I/O, filesystem and the webserver and we're done.
 #if ESP32
-void app_main(void) {
+void app_main(void)
+{
 #else
-void user_init(void) {
+void user_init(void)
+{
 #endif
 
 #ifndef ESP32
@@ -455,8 +480,8 @@ void user_init(void) {
 #endif
 
 	ioInit();
-// FIXME: Re-enable this when capdns is fixed for esp32
-//	captdnsInit();
+	// FIXME: Re-enable this when capdns is fixed for esp32
+	//	captdnsInit();
 	esp_err_t err;
 	// Init NVS
 	err = nvs_flash_init();
@@ -464,12 +489,13 @@ void user_init(void) {
 	{
 		// NVS partition was truncated and needs to be erased
 		// Retry nvs_flash_init
+		ESP_LOGW(TAG, "NVS invalid, reformatting... ");
 		ESP_ERROR_CHECK(nvs_flash_erase());
 		err = nvs_flash_init();
 	}
-	ESP_ERROR_CHECK(err);
+	ESP_ERROR_CHECK(err); // will reboot if error!
 
-	uint32_t net_configured = 0;	 // value will default to 0, if not set yet in NVS
+	uint32_t net_configured = 0; // value will default to 0, if not set yet in NVS
 	ESP_LOGI(TAG, "Opening NVS handle ");
 	err = nvs_open(NVS_NAMESPACE, NVS_READWRITE, &my_nvs_handle);
 	if (err != ESP_OK)
@@ -499,31 +525,30 @@ void user_init(void) {
 	EspFsConfig espfs_conf = {
 		.memAddr = espfs_image_bin,
 	};
-	EspFs* fs = espFsInit(&espfs_conf);
-    httpdRegisterEspfs(fs);
+	EspFs *fs = espFsInit(&espfs_conf);
+	httpdRegisterEspfs(fs);
 #endif // CONFIG_ESPHTTPD_USE_ESPFS
 
 	tcpip_adapter_init();
 	httpdFreertosInit(&httpdFreertosInstance,
-	                  builtInUrls,
-	                  LISTEN_PORT,
-	                  connectionMemory,
-	                  MAX_CONNECTIONS,
-	                  HTTPD_FLAG_NONE);
+					  builtInUrls,
+					  LISTEN_PORT,
+					  connectionMemory,
+					  MAX_CONNECTIONS,
+					  HTTPD_FLAG_NONE);
 	httpdFreertosStart(&httpdFreertosInstance);
 
-
 	xTaskCreate(websocketBcast, "wsbcast", 3000, NULL, 3, NULL);
-	
-	ESP_ERROR_CHECK(initCgiWifi()); // Initialise wifi configuration CGI
+
+	initCgiWifi(); // Initialise wifi configuration CGI
 
 	ESP_ERROR_CHECK(esp_event_loop_init(app_event_handler, NULL));
-	
-#ifdef ETHERNET_ENABLE
-	init_ethernet();
-#endif
 
 	init_wifi(!net_configured); // Start Wifi, restore factory wifi settings if not initialized
+	startCgiWifi();				// will try to restore STA connection on boot
+#ifdef MY_ETH_ENABLE
+	init_ethernet();
+#endif
 
 	if (!net_configured)
 	{ // If wasn't initialized, now we are initialized.  Write it to NVS.
